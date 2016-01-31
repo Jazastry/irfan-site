3f54889a735ddeb1ad6bcfe10a8fbf440829e359
Jazastry <yogib@mail.bg>
2016-01-31 23:12:33 +0200

    ...

--- source/js/controllers/default_ctrl.js
+++ source/js/controllers/default_ctrl.js
@@ -2,11 +2,12 @@ app.controller('DefaultCtrl', ['$scope', '$timeout', function($scope, $timeout) 
     $scope.roseteClass = 'rotatingbehind';
     $scope.fadeInAnimation = 'opacity_zero';
 
-    $timeout(function() {
-        $scope.fadeInAnimation = 'fadein';
-    }, 500);
+    // $timeout(function() {
+    //     $scope.fadeInAnimation = 'fadein';
+    // }, 500);
 
     $timeout(function() {
-        $scope.roseteClass = 'rotating';
-    }, 1000);
+        // $scope.roseteClass = 'rotating';
+        $scope.fadeInAnimation = 'fadein';
+    }, 1500);
 }]);
