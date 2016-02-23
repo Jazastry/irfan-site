9c485ecfa22f9d3324fe91c408b5a89812e0a485
Jazastry <jazastry@gmail.com>
2016-02-23 11:05:37 +0200

    ...

--- source/server/server_modules/events.js
+++ source/server/server_modules/events.js
@@ -1,18 +1,20 @@
 module.exports = (function() {
     function Events() {
         this.events = [];
-        this.facebookRequester = require('./facebook_requester_es6.js');
+        this.facebookRequester = require('./fb_req_es6.js');
         this.loadEvents();
     }
 
-    Events.prototype.loadEvents = function(callback) {
+    Events.prototype.loadEvents = function() {
         var _this = this;
 
         _this.facebookRequester.getEvents()
             .then(function(events) {
+            console.log('events ' , events);
                 _this.events = events;
             });
-        _this.listenForDataChanges();
+
+        // _this.listenForDataChanges();
     };
 
     Events.prototype.listenForDataChanges = function() {
